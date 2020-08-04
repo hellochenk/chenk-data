84f04bd1ff0df88548ca843a67d77ddf4f3345f1
chenk <657301050@qq.com>
2020-08-04 18:01:10 +0800

    Update bubblesort.js

--- sort/bubblesort.js
+++ sort/bubblesort.js
@@ -1,8 +1,8 @@
-```js
-  const bubbleSort = arr => {
-    const len = arr.length;
-    for(let outer = len - 1; outer >= 1; outer--) {
-      // todo continue
-    }
+
+const bubbleSort = arr => {
+  const len = arr.length;
+  for(let outer = len - 1; outer >= 1; outer--) {
+    // todo continue
   }
-```
+}
+
